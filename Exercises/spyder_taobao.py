81a34af11ec0ba52e7828b4a3f7ac2411f2ec548
ChiaSang <vic.zkj@gmail.com>
2017-12-28 20:49:39 +0800

    完成淘宝爬虫(bug带修复)

--- Exercises/spyder_taobao.py
+++ Exercises/spyder_taobao.py
@@ -12,6 +12,7 @@ from hashlib import md5
 import requests
 
 t = time.localtime()
+
 # 数据
 DATA =[]
 
@@ -21,26 +22,28 @@ DATA =[]
 #     f.write(txt)
 #     f.close()
 
-
 # 参数
 find_arg = {
-    'q': 'python',
+    'q': '陀螺仪',
     'initiative_id': 'staobaoz_%s%02d%02d' % (t[0], t[1], t[2]),
-      '&ie': 'utf8'
+    '&ie': 'utf8'
 }
-search = 'go'
+
+# ajax请求
+ajax_arg = {
+    '_ksTs': '',
+    'callback': '',
+      'data-calue': ''
+}
+search = '键盘'
 time_id = 'staobaoz_%s%02d%02d' % (t[0], t[1], t[2])
-# print(time_id)
 
 # 搜索页面
 first_url = 'https://s.taobao.com/search?q=' + str(search)
 second_url = '&imgfile=&js=1&stats_click=search_radio_all%3A1&initiative_id=' + str(time_id)
 third_url = first_url + second_url + '&ie=utf8'
-# print('third_url:\n', third_url)
-r = requests.get(third_url)
-print('third_url_r_url:\n{0}\n'.format(r.url))
+r = requests.get(third_url, params=find_arg)
 html = r.text
-# print(html)
 
 # 分析页面
 content = re.findall(r'g_page_config = (.*?)g_srp_loadCss', html, re.S)[0][:-6]
@@ -48,7 +51,7 @@ content = re.findall(r'g_page_config = (.*?)g_srp_loadCss', html, re.S)[0][:-6]
 
 # 转换字典
 content = json.loads(content)
-print('{}'.format(content))
+print('content:\n', content)
 data_list = content['mods']['itemlist']['data']['auctions']
 
 # 提取数据
@@ -95,3 +98,36 @@ for item in data_list:
 # 更新cookies
 cookie_ = r2.cookies
 print(len(data_list))
+
+# 爬取剩下的页面
+for i in range(1, 10):
+    ksts = time.time()
+    ajax_arg['_ksTs'] = '%s_%s' % (int(ksts * 1000), str(ktsts)[-3:])
+    ajax_arg['callback'] = 'jsonp%d' % (int(str(ktsts)[-3:] + 1))
+    ajax_arg['data-calue'] = 44 *i
+    url = '''
+    https://s.taobao.com/search?data-key=s&data-value=132&ajax=true&imgfile=&js=1&stats_click=search_radio_all%3A1&ie=utf8&bcoffset=4&ntoffset=4&p4ppushleft=1%2C48
+    '''.format(time.time())
+    print(url)
+    if i > 1:
+        find_arg['s'] = 44 *(i - 1)
+    r3 = requests.get(url, params=find_arg, cookies=cookie_)
+    html = r3.text
+    data_list = json.loads(re.findall(r'{.*}', html)[0])['mods']['itemlist']['data']['auctions']
+
+    # 数据
+    for item in data_list:
+        temp = {
+            'title': item['title'],
+            'view_price': item['view_price'],
+            'view_sales': item['view_sales'],
+            'view_fee': '否' if float(item['view_fee']) else '是',
+            'isTmall': '是' if item['shopcard']['isTmall'] else '否',
+            'name': item['nick'],
+            'area': item['item_loc'],
+            'detail_url': item['detail_url']
+        }
+        DATA.append(temp)
+cookie_ = r3.cookies
+print(html)
+# 写入
