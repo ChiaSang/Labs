289f118ee86333394ba4a021db59bfbb654ad506
ChiaSang <vic.zkj@gmail.com>
2018-01-26 18:02:51 +0800

    print the format str

--- Exercises/execute_excel.py
+++ Exercises/execute_excel.py
@@ -6,8 +6,13 @@ Created on Fri Jan 26 16:16:51 2018
 """
 
 import openpyxl
+name = []
 wb = openpyxl.load_workbook(r'E:\Google 云端硬盘\Doc\2#宿舍楼ip地址分配表2016.xlsx')
 sheet1 = wb.sheetnames
 info_sheet1 = wb['Sheet1']
 for i in info_sheet1['B']:
-    print(i.value)
+#    print(i.value)
+    name.append(str(i.value))
+name = sorted(set(name))
+for item in name:
+    print('{:^5}'.format(item)) # 中间对齐，默认5个宽度
