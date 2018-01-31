0e06a407c273ad3760103335a45765edf8756146
ChiaSang <vic.zkj@gmail.com>
2018-01-31 16:41:27 +0800

    修复bug，完善功能

--- Exercises/spyder_taobao.py
+++ Exercises/spyder_taobao.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
 Created on Thu Dec 21 17:29:47 2017
@@ -10,123 +11,126 @@ import time
 import json
 from hashlib import md5
 import requests
-
-t = time.localtime()
+import xlwt
 
 # 数据
-DATA = []
+PRODUCTION_DATA = []
 
-# def store_file(filename, txt):
-#     # print"正在存储......"
-#     f = open(filename, 'w')
-#     f.write(txt)
-#     f.close()
-
+TIME_ARGS = time.localtime()
+# 搜索关键字
+searchKey = '内衣'
 # 参数
-find_arg = {
-    'q': '袜子',
-    'initiative_id': 'staobaoz_%s%02d%02d' % (t[0], t[1], t[2]),
-    '&ie': 'utf8'
+FIND_ARGS = {
+    'q':
+    searchKey,
+    'initiative_id':
+    'staobaoz_%s%02d%02d' % (TIME_ARGS[0], TIME_ARGS[1], TIME_ARGS[2])
 }
-
-# ajax请求
-ajax_arg = {'_ksTs': '', 'callback': '', 'data-calue': ''}
-search = '袜子'
-time_id = 'staobaoz_%s%02d%02d' % (t[0], t[1], t[2])
-
 # 搜索页面
-first_url = 'https://s.taobao.com/search?q=' + str(search)
-second_url = '&imgfile=&js=1&stats_click=search_radio_all%3A1&initiative_id=' + str(
-    time_id)
-third_url = first_url + second_url + '&ie=utf8'
-r = requests.get(third_url, params=find_arg)
-html = r.text
-
+BASE_URL = "https://s.taobao.com/search?imgfile=&js=1&stats_click=search_radio_all%3A1&ie=utf8"
+# 发送请求
+WEB = requests.get(BASE_URL, params=FIND_ARGS)
+HTML = requests.get(BASE_URL, params=FIND_ARGS).text
 # 分析页面
-content = re.findall(r'g_page_config = (.*?)g_srp_loadCss', html, re.S)[0][:-6]
-
+CONTENT = re.findall(r'g_page_config = (.*?)g_srp_loadCss', HTML, re.S)[0][:-6]
 # 转换字典
-content = json.loads(content)
-print('content:\n', content)
-data_list = content['mods']['itemlist']['data']['auctions']
+CONTENT = json.loads(CONTENT)
+SEARCH_DATA_LIST = CONTENT['mods']['itemlist']['data']['auctions']
 
 # 提取数据
-for item in data_list:
+for item in SEARCH_DATA_LIST:
     temp = {
         'title': item['title'],
         'view_price': item['view_price'],
         'view_sales': item['view_sales'],
         'view_fee': '否' if float(item['view_fee']) else '是',
         'isTmall': '是' if item['shopcard']['isTmall'] else '否',
-        'name': item['nick'],
         'area': item['item_loc'],
-        'detail_url': item['detail_url']
+        'name': item['nick'],
+        'detail_url': item['detail_url'],
     }
-    DATA.append(temp)
+    PRODUCTION_DATA.append(temp)
 
 # 保存cookies
-cookie_ = r.cookies
+COOKIE_ = WEB.cookies
 
 # 异步加载页面数据
 ksts = str(int(time.time() * 1000))
-url = '''
-https://s.taobao.com/api?_ksTS={}_208&callback=jsonp209&ajax=true&m=customized&stats_click=search_radio_all:1&q=%E5%AD%A3%E7%BE%A1%E6%9E%97&s=36&imgfile=&initiative_id=staobaoz_20171222&bcoffset=0&js=1&ie=utf8&rn={}
-'''.format(ksts,
-           md5(ksts.encode()).hexdigest())
+url = "https://s.taobao.com/api?_ksTS={}_208&callback=jsonp209&ajax=true&m=customized&stats_click=search_radio_all:1&q=&imgfile=&bcoffset=0&js=1&ie=utf8&rn={}".format(
+    ksts, md5(ksts.encode()).hexdigest())
 
-r2 = requests.get(url, params=find_arg, cookies=cookie_)
-html = r2.text
-data_list = json.loads(re.findall(
-    r'{.*}', html)[0])['API.CustomizedApi']['itemlist']['auctions']
+r2 = requests.get(url, params=FIND_ARGS, cookies=COOKIE_)
+HTML = r2.text
+SEARCH_DATA_LIST = json.loads(re.findall(
+    r'{.*}', HTML)[0])['API.CustomizedApi']['itemlist']['auctions']
 
 # 提取数据
-for item in data_list:
+for item in SEARCH_DATA_LIST:
     temp = {
         'title': item['title'],
         'view_price': item['view_price'],
         'view_sales': item['view_sales'],
         'view_fee': '否' if float(item['view_fee']) else '是',
         'isTmall': '是' if item['shopcard']['isTmall'] else '否',
-        'name': item['nick'],
         'area': item['item_loc'],
-        'detail_url': item['detail_url']
+        'name': item['nick'],
+        'detail_url': item['detail_url'],
     }
-    DATA.append(temp)
+    PRODUCTION_DATA.append(temp)
 
 # 更新cookies
-cookie_ = r2.cookies
-print(len(data_list))
-
+COOKIE_ = r2.cookies
 # 爬取剩下的页面
 for i in range(1, 10):
-    ksts = time.time()
-    ajax_arg['_ksTs'] = '%s_%s' % (int(ksts * 1000), str(ktsts)[-3:])
-    ajax_arg['callback'] = 'jsonp%d' % (int(str(ktsts)[-3:] + 1))
-    ajax_arg['data-calue'] = 44 * i
-    url = '''
-    https://s.taobao.com/search?data-key=s&data-value=132&ajax=true&imgfile=&js=1&stats_click=search_radio_all%3A1&ie=utf8&bcoffset=4&ntoffset=4&p4ppushleft=1%2C48
-    '''.format(time.time())
+    ktsts = time.time()
+    FIND_ARGS['_ksTS'] = '%s_%s' % (int(ktsts * 1000), str(ktsts)[-3:])
+    FIND_ARGS['callback'] = 'jsonp%d' % (float(str(ktsts)[-3:]) + 1)
+    FIND_ARGS['data-value'] = 44 * i
+    url = "https://s.taobao.com/search?data-key=s&data-value=44&ajax=true&imgfile=&js=1&stats_click=search_radio_all%3A1&ie=utf8&bcoffset=4&ntoffset=4&p4ppushleft=1%2C48".format(
+        time.time())
     print(url)
     if i > 1:
-        find_arg['s'] = 44 * (i - 1)
-    r3 = requests.get(url, params=find_arg, cookies=cookie_)
-    html = r3.text
-    data_list = json.loads(re.findall(
-        r'{.*}', html)[0])['mods']['itemlist']['data']['auctions']
+        FIND_ARGS['s'] = 44 * (i - 1)
+    r3 = requests.get(url, params=FIND_ARGS, cookies=COOKIE_)
+    HTML = r3.text
+    SEARCH_DATA_LIST = json.loads(re.findall(
+        r'{.*}', HTML)[0])['mods']['itemlist']['data']['auctions']
 
     # 数据
-    for item in data_list:
+    for item in SEARCH_DATA_LIST:
         temp = {
             'title': item['title'],
             'view_price': item['view_price'],
             'view_sales': item['view_sales'],
             'view_fee': '否' if float(item['view_fee']) else '是',
             'isTmall': '是' if item['shopcard']['isTmall'] else '否',
-            'name': item['nick'],
             'area': item['item_loc'],
-            'detail_url': item['detail_url']
+            'name': item['nick'],
+            'detail_url': item['detail_url'],
         }
-        DATA.append(temp)
-cookie_ = r3.cookies
-print(html)
-# 写入
+        PRODUCTION_DATA.append(temp)
+COOKIE_ = r3.cookies
+# 持久化
+f = xlwt.Workbook(encoding='utf-8')
+sheet01 = f.add_sheet(u'sheet1', cell_overwrite_ok=True)
+# 写标题
+sheet01.write(0, 0, '商品名')
+sheet01.write(0, 1, '标价')
+sheet01.write(0, 2, '购买人数')
+sheet01.write(0, 3, '是否包邮')
+sheet01.write(0, 4, '是否天猫')
+sheet01.write(0, 5, '地区')
+sheet01.write(0, 6, '店名')
+sheet01.write(0, 7, 'url')
+# 写内容
+for i in range(len(PRODUCTION_DATA)):
+    sheet01.write(i + 1, 0, PRODUCTION_DATA[i]['title'])
+    sheet01.write(i + 1, 1, PRODUCTION_DATA[i]['view_price'])
+    sheet01.write(i + 1, 2, PRODUCTION_DATA[i]['view_sales'])
+    sheet01.write(i + 1, 3, PRODUCTION_DATA[i]['view_fee'])
+    sheet01.write(i + 1, 4, PRODUCTION_DATA[i]['isTmall'])
+    sheet01.write(i + 1, 5, PRODUCTION_DATA[i]['area'])
+    sheet01.write(i + 1, 6, PRODUCTION_DATA[i]['name'])
+    sheet01.write(i + 1, 7, PRODUCTION_DATA[i]['detail_url'])
+
+f.save(u'搜索%s的结果.xls' % searchKey)
