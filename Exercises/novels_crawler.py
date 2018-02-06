75a818a2df8c70b7b0f14c242891c6b14777efd2
ChiaSang <vic.zkj@gmail.com>
2018-02-06 14:07:41 +0800

    Initial commit

---
+++ Exercises/novels_crawler.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Created by ChiaSang on 2018/2/5
+
+
+import  requests
+import re
+from bs4 import BeautifulSoup
+pages = 10
+se_num = re.compile(r'\d+')
+for page in range(1, pages):
+    url = 'https://www.sto.cc/book-112356-{0}.html'.format(str(page))
+    html = requests.get(url)
+    soup = BeautifulSoup(html.text, 'lxml')
+    now_page = soup.select('div.bookbox > h1')
+    num = se_num.search(str(now_page[0].get_text()))
+    body = soup.select('#BookContent')
+    raw_novel = ''.join(body[0].get_text())
+    with open('何为贤妻.txt', 'a', encoding='utf-8') as f:
+        f.write(raw_novel)
