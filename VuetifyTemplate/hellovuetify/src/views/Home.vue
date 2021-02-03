dcb0a0bf4e644d6b213d7294f5728516a1a6826e
Victor <vic.zkj@gmail.com>
2021-02-03 17:44:24 +0800

    Initial Commit

---
+++ VuetifyTemplate/hellovuetify/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home purple darken-2 text-center">
+    <h1 class="display-1">HelloWorld</h1>
+    <p class="green white--text">
+      One should give up anger; one should abandon pride; one should overcome
+      all fetters. I’ll never befall him who clings not to mind and body and is
+      passionless.
+    </p>
+    <p>
+      一个人应该舍弃愤怒，拔除傲慢，超越所有的束缚。不执着心灵和物质的人，内心可以得到真正的安宁，而不受外在的影响。
+    </p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {};
+</script>
+
